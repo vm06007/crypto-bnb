0bddfe675b289762b5c5fa19607b750e2d89df3c
Vitally Marinchenko <vitally.marinchenko@gmail.com>
2025-10-01 01:55:18 +0800

    setup improved

---
+++ evm/test/mocks/MockERC20.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ERC20} from "solady/tokens/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    uint8 private _decimals;
+    string private _name;
+    string private _symbol;
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_) {
+        _name = name_;
+        _symbol = symbol_;
+        _decimals = decimals_;
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
+
+    function name() public view override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view override returns (string memory) {
+        return _symbol;
+    }
+}
